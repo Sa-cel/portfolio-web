13c544cddf5e85d7de1c43643c7675c5eb6a3a86
Sa-cel <116497602+Sa-cel@users.noreply.github.com>
2024-07-22 17:58:06 +0300

    ic

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,8 +7,8 @@ const Portfolio = () => (
     <div>
     <h3>Portfolio-website Project</h3>
     <p>This project showcases my portfolio website, including details about my education, experience, skills, and contact information.</p>
-      <a href="https://link-to-project.com">Live Demo</a> |
-      <a href="https://github.com/yourprofile/project-repo">GitHub Repo</a>
+      <a href="http://localhost:3001/">Live Demo</a> |
+      <a href="https://github.com/Sa-cel/portfolio-web.git">GitHub Repo</a>
     </div>
     {/* Repeat for more projects */}
   </section>
